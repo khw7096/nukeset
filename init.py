6381e4d260b55290186de40c4082d53097c2aded
woong <hello@lazypic.org>
2019-02-18 16:54:53 +0900

    add lut

--- init.py
+++ init.py
@@ -1,7 +1,11 @@
 #coding:utf8
 import nuke
+import os
 
 nuke.pluginAddPath("./gizmos", addToSysPath=True)
 nuke.pluginAddPath("./images", addToSysPath=True)
 nuke.pluginAddPath("./lib", addToSysPath=True)
 nuke.pluginAddPath("./scripts", addToSysPath=True)
+
+nukePath = os.environ["NUKE_PATH"]
+nuke.ViewerProcess.register("AlexaV3LogC", nuke.createNode, ("Vectorfield", "vfield_file %s/luts/ARRI_LogC2Video_Classic709_davinci3d.cube colorspaceIn AlexaV3LogC" % (nukePath)))
