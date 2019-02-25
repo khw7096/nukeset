cbd312007ed6f9f90ff106c96598ee0aaaa76def
woong <hello@lazypic.org>
2019-02-25 11:18:32 +0900

    add nklibrary

--- menu.py
+++ menu.py
@@ -3,6 +3,7 @@ import nukescripts
 import checkenv
 import openfile
 import makewrite
+import nklibrary
 
 tb = nuke.toolbar("Nodes")
 m = tb.addMenu("Lazypic", icon="lazypic_logo.png")
@@ -19,3 +20,4 @@ mb.addCommand("-","","")
 mb.addCommand("CheckENV", "checkenv.main()")
 mb.addCommand("OpenFile", "reload(openfile);openfile.main()", "F8", shortcutContext=2)
 mb.addCommand("MakeWrite", "reload(makewrite);makewrite.main()", "F10", shortcutContext=2)
+mb.addCommand("Nklibrary", "reload(nklibrary);nklibrary.main()")
